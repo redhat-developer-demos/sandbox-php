a62f1f9a83e9e2ddfc4f3023c71cfd63ef8d24c7
Don Schenck <dschenck@redhat.com>
2025-07-11 14:24:02 -0400

    Simplified

--- index.php
+++ index.php
@@ -1,16 +1,60 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
- */
+<!DOCTYPE html>
+<html lang="en">
 
-require 'webroot' . DIRECTORY_SEPARATOR . 'index.php';
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome to My Page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-light">
+  <div class="container">
+    <main role="main" class="pb-3">
+      <h1 class="display-4">Welcome to Red Hat OpenShift</h1>
+      <p>The Developer Sandbox allows you get free access to an OpenShift cluster for 30 days. Learn Kubernetes,
+        experiment with microservics, or build a Proof of Concept for your organization.</p>
+      <p>Explore the following technologies and concepts using the Developer Sandbox Learning Paths we've created:</p>
+      <div class="card-group">
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="/img/ST-serverless_large.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Activity</h5>
+            <p class="card-text">Create an OpenShift Serverless function</p>
+            <a href="https://developers.redhat.com/developer-sandbox/activities/create-openshift-serverless-function"
+              class="btn btn-primary">Take me there</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="/img/petclinic-card.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Activity</h5>
+            <p>Deploy a Java Application on Kubernetes in minutes</p>
+            <a href="https://developers.redhat.com/developer-sandbox/activities/how-to-deploy-java-application-in-kubernetes"
+              class="btn btn-primary">Take me there</a>
+          </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="/img/kube-activity-card.png" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Activity</h5>
+            <p>Learn Kubernetes</p>
+            <a href="https://developers.redhat.com/developer-sandbox/activities/learn-kubernetes-using-red-hat-developer-sandbox-openshift"
+              class="btn btn-primary">Take me there</a>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <main role="main" class="pb-3">
+        </main>
+      </div>
+
+      <footer class="border-top footer text-muted">
+        <div class="container">
+          github.com/redhat-developer-demos/sandbox-php
+        </div>
+      </footer>
+    </main>
+  </div>
+</body>
+
+</html>
